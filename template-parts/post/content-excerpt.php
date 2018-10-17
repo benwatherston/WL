2c0e0d46b5d6b249388f4460f0a8cd6d10c2a5f1
Ben Watherston <ben@cleversteam.com>
2018-10-17 22:46:47 +0100

    Update tags/search

--- template-parts/post/content-excerpt.php
+++ template-parts/post/content-excerpt.php
@@ -14,9 +14,16 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>"  <?php post_class("excerpt-wrap"); ?>>
 
 	<header class="entry-header">
+  <?php if ( is_front_page() && ! is_home() ) {
+
+			// The excerpt is being displayed within a front page section, so it's a lower hierarchy than h2.
+			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
+		} else {
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		} ?>
 		<?php if ( 'post' === get_post_type() ) : ?>
 			<div class="entry-meta">
 				<?php
@@ -30,13 +37,7 @@
 			</div><!-- .entry-meta -->
 		<?php endif; ?>
 
-		<?php if ( is_front_page() && ! is_home() ) {
 
-			// The excerpt is being displayed within a front page section, so it's a lower hierarchy than h2.
-			the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
-		} else {
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-		} ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
